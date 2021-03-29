db25172744e51640024e8a636a1e4b10dbb0fd65
Shinya Kitamura <shinya.kitamura.14@gmail.com>
2021-03-29 13:27:12 +0200

    update

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 const StopWatch = props => {
   const [timer, setTimer] = useState(0)
